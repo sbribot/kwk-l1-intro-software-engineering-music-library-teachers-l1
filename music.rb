bcfb5e140e8fbbd7cfddc99400eb1d2acfeb7ad9
Sara Beth Ribot <sbribot@gmail.com>
2018-06-06 03:34:06 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -40,9 +40,13 @@ end
 #       :maroon_five => "Sugar"
 #     }
 
-4. This method should add a new artist and song (key value pair) to the hash and return the updated hash. 
-This method should accept three arguments, the library hash, the artist, and the song.
+# 4. This method should add a new artist and song (key value pair) to the hash and return the updated hash. 
+# This method should accept three arguments, the library hash, the artist, and the song.
 
+def add_song_and_artist(library,artist,song)
+  library.add(artist,song)
+  return library 
+end 
 
 
 
