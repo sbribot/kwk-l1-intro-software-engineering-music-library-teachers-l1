729479e354cf07a8be30aabd67053699c2701eac
Sara Beth Ribot <sbribot@gmail.com>
2018-06-06 03:35:27 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -44,7 +44,7 @@ end
 # This method should accept three arguments, the library hash, the artist, and the song.
 
 def add_song_and_artist(library,artist,song)
-  library.merge(artist,song)
+  library.merge(artist => song)
   return library 
 end 
 
